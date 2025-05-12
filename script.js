9c0923587dec84ccdd8703fd4fe72e6a4dd815e4
fisalazarm <80424265+fisalazarm@users.noreply.github.com>
2025-05-12 20:55:47 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-document.getElementById('openLetter').addEventListener('click', () => {
-  document.querySelector('.card').classList.add('hidden');
-  document.querySelector('.letter').classList.remove('hidden');
-});
-
-document.getElementById('closeLetter').addEventListener('click', () => {
-  document.querySelector('.letter').classList.add('hidden');
-  document.querySelector('.card').classList.remove('hidden');
+document.getElementById('envelope').addEventListener('click', function() {
+  this.classList.toggle('open');
 });
